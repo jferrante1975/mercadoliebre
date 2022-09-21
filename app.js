b44c61f0f5254d0195e908a970794fc20ece9c3e
jferrante1975 <javierferrante@gmail.com>
2022-09-20 23:54:35 -0300

    subiendo imagenes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+
+    res.sendFile(path.join(__dirname, './src/views/home.html'));
+
+})
+
+app.listen(3030, () => {
+    console.log('Inicializado el servidor');
+})
